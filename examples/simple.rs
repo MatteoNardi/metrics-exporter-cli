466ba4b23233d58f8c33487a88eb9350dd7c79b8
Matteo Nardi <matteo@exein.io>
2023-04-27 12:32:41 +0200

    new: add example

---
+++ examples/simple.rs
@@ -0,0 +1,25 @@
+use std::time::Duration;
+
+use metrics::counter;
+use metrics_exporter_cli::CliRegister;
+
+fn main() {
+    let register = CliRegister::install().expect("Error installing register");
+    std::thread::spawn(|| {
+        let mut iterations = 0;
+        counter!("group2", 42);
+        loop {
+            counter!("group1.val_a", iterations * 10);
+            counter!("group1.val_b", iterations * 7);
+            iterations += 1;
+            std::thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    std::thread::sleep(Duration::from_secs(1));
+    println!("{}", register.header());
+    loop {
+        println!("{}", register.status());
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
